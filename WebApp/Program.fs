2f84037184d1637304ffba4bccf0e9df58224c53
Andrew Grant <andy.a.grant@gmail.com>
2019-07-29 22:00:38 +0100

    Added in FsConfig for configuration

--- WebApp/Program.fs
+++ WebApp/Program.fs
@@ -5,7 +5,10 @@ open System.IO
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
+open FsConfig
 open Giraffe
+
+open InfrastructureTypes
 
 // ---------------------------------
 // Web app
@@ -16,6 +19,21 @@ let webApp = text "Hello world"
 // ---------------------------------
 // Config and Main
 // ---------------------------------
+
+let config =
+    match EnvConfig.Get<Config>() with
+    | Ok config -> config
+    | Error error ->
+        match error with
+        | NotFound envVarName ->
+            failwithf "Environment variable %s not found" envVarName
+        | BadValue (envVarName, value) ->
+            failwithf "Environment variable %s has invalid value %s" envVarName value
+        | NotSupported msg ->
+            failwith msg
+
+let endpoint = BingEndpoint <| sprintf "https://%s.api.cognitive.microsoft.com" config.Endpoint
+let apiKey = BingApiKey config.ApiKey
 
 let configureApp (app : IApplicationBuilder) =
     app.UseDeveloperExceptionPage()
